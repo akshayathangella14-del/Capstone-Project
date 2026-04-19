d7f34fdcccb2d37a9b9ca64d092bfaf2df1a3783
unknown <akshayathangella14@gmail.com>
2026-04-19 22:05:20 +0530

    Updated VerifyToken to support Authorization headers

--- backend/middlewares/VerifyToken.js
+++ backend/middlewares/VerifyToken.js
@@ -8,7 +8,12 @@ config();
 export const verifyToken = (...allowedRoles) => {
   return async (req, res, next) => {
     try {
-      const token = req.cookies?.token;
+      // Look for token in Cookies OR Authorization Header
+let token = req.cookies?.token;
+
+    if (!token && req.headers.authorization) {
+    token = req.headers.authorization.split(" ")[1]; 
+    }
 
       if (!token) {
         return res.status(401).json({ message: "Please login" });
